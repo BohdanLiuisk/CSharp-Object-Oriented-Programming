510778d1a13bcc2e83ab09edb6b45fb879e59a74
Bohdan <bohdanliusik@gmail.com>
2020-06-17 17:57:07 +0300

    Added classes Address and Rectangle

---
+++ Solution/Rectangle/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first rectangle side: ");
+            double side1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter second rectangle side: ");
+            double side2 = Convert.ToDouble(Console.ReadLine());
+
+            Rectangle rectangle = new Rectangle(side1, side2);
+            Console.WriteLine("Perimeter = {0}, Area = {1}", rectangle.Perimeter, rectangle.Area);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Solution/Rectangle/Rectangle.cs
@@ -0,0 +1,38 @@
+﻿namespace Rectangle
+{
+    class Rectangle
+    {
+        double side1, side2;
+
+        public Rectangle(double side1, double side2)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+        }
+
+        double PerimeterCalculator()
+        {
+            return (side1 + side2) * 2;
+        }
+        public double Perimeter
+        {
+            get
+            {
+                return this.PerimeterCalculator();
+            }
+        }
+
+        double AreaCalculator()
+        {
+            return side1 * side2;
+        }
+
+        public double Area
+        {
+            get
+            {
+                return this.AreaCalculator();
+            }
+        }
+    }
+}
