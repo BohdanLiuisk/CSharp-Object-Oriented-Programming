fcb7cd529aa957960a37d4686699735d684cd7be
BohdanLiusik <bodialiusik@gmail.com>
2020-06-17 18:45:06 +0300

    Added class to convert currency

---
+++ Solution/Currency Converter/Converter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Currency_Converter
+{
+    class Converter
+    {
+        double usd, eur, uan;
+
+        public Converter(double usd, double eur, double uan)
+        {
+            this.usd = usd;
+            this.eur = eur;
+            this.uan = uan;
+        }
+
+        public void ToUsd(double uahSum)
+        {
+            Console.WriteLine(uahSum / usd);
+        }
+
+        public void FromUsd(double usdSum)
+        {
+            Console.WriteLine(usdSum * usd);
+        }
+
+        public void ToEur(double uahSum)
+        {
+            Console.WriteLine(uahSum / eur);
+        }
+
+        public void FromEur(double eurSum)
+        {
+            Console.WriteLine(eurSum * usd);
+        }
+
+        public void ToUan(double uahSum)
+        {
+            Console.WriteLine(uahSum / uan);
+        }
+
+        public void FromUan(double rubSum)
+        {
+            Console.WriteLine(rubSum * usd);
+        }
+    }
+}
---
+++ Solution/Currency Converter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Currency_Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Converter converter = new Converter(26.7, 29.9, 1);
+
+            converter.ToUsd(1);
+            converter.FromUsd(10);
+
+            Console.ReadKey();
+        }
+    }
+}
