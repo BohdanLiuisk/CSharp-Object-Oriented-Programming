05c4eca76202acff8bc458683bb2bf6303742af0
Bohdan <bohdanliusik@gmail.com>
2020-06-17 18:16:15 +0300

    Added class Figure and Point to determine the type of figure

---
+++ Solution/Figure/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Figure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Figure figure = new Figure(new Point("A", 1, 1), new Point("B", 1, 4), new Point("C", 4, 4));
+
+            Console.Write("{0}, \nPerimetr = ", figure.Type);
+
+            figure.PerimeterCalculator();
+
+            Console.ReadKey();
+        }
+    }
+}
