311cb479153df9f1da2584cf9108fb22d04e4033
BohdanLiusik <bodialiusik@gmail.com>
2020-06-18 17:17:24 +0300

    class Employee

---
+++ Solution/Employee/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Employee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee("Smith", "Petr");
+            employee.Post = "manager";
+            employee.Experience = 1;
+            
+            Console.WriteLine(employee.Surname + " " + employee.Name + " " + employee.Post.ToUpper());
+            employee.ShowSalary();
+
+            Console.ReadKey();
+        }
+    }
+}
