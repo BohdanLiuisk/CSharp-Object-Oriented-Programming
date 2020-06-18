0d75342005b5f60e0c8ea44d976c65fca471a775
BohdanLiusik <bodialiusik@gmail.com>
2020-06-18 18:27:39 +0300

    class Invoice

---
+++ Solution/Invoice/Invoice.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Invoice
+{
+    class Invoice
+    {
+        public readonly int account = 0;
+        public readonly string customer = null;
+        public readonly string provider = null;
+
+        public string Article { get; set; }
+
+        public int Quantity { get; set; }
+
+        public Invoice(int account, string customer, string provider)
+        {
+            this.account = account;
+            this.customer = customer;
+            this.provider = provider;
+        }
+        public void CostCalculation(bool needNds)
+        {
+            double cost;
+            switch (Article)
+            {
+                case "laptop":
+                    cost = 5400;
+                    break;
+                case "SD-cadr":
+                    cost = 30;
+                    break;
+                case "USB-hab":
+                    cost = 12;
+                    break;
+                default:
+                    Console.WriteLine("There is no information about this product.");
+                    return;
+            }
+            if (needNds)
+            {
+                cost = cost * 7 / 6;
+            }
+            Console.WriteLine("Amount of payment: {0}$", cost * Quantity);
+        }
+    }
+}
---
+++ Solution/Invoice/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Invoice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Invoice inv = new Invoice(678904, "Alex", "Foxtrot") 
+            { 
+                Article = "USB-hab", 
+                Quantity = 6 
+            };
+
+            inv.CostCalculation(true);
+            inv.CostCalculation(false);
+
+            Console.ReadKey();
+        }
+    }
+}
