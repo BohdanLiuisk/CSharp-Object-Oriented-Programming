2f11973d98f4691b7886899569bf5427810071aa
BohdanLiusik <bodialiusik@gmail.com>
2020-06-21 22:20:09 +0300

    class Document with all Parts

---
+++ Solution/DocumentParts/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DocumentParts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Document document = new Document("Контракт");
+            document.Body = "Тело контракта...";
+            document.Footer = "Директор: Иванов И.И.";
+
+            document.Show();
+
+            Console.ReadKey();
+        }
+    }
+}
