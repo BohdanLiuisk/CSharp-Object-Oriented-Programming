8e90647c3e9705d1d89e78722009f3240f1c2913
BohdanLiusik <bodialiusik@gmail.com>
2020-06-21 22:29:14 +0300

    class AbstractHandler

---
+++ Solution/AbstractHandler/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace AbstractHandler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Redactor redactor = new Redactor();
+            redactor.ChooseDocument("file.doc");
+
+            redactor.Open();
+            redactor.Change();
+            redactor.Save();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Solution/AbstractHandler/Redactor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace AbstractHandler
+{
+    class Redactor
+    {
+        AbstractHandler handler;
+
+        public void ChooseDocument(string fileName)
+        {
+            string format = fileName.Substring(fileName.Length - 4);
+
+            switch (format.ToLower())
+            {
+                case ".txt":
+                    handler = new TXTHandler(fileName);
+                    break;
+                case ".doc":
+                    handler = new DOCHandler(fileName);
+                    break;
+                case ".xml":
+                    handler = new XMLHandler(fileName);
+                    break;
+                default:
+                    Console.WriteLine("Неопознаный формат");
+                    break;
+            }
+        }
+
+        public void Open()
+        {
+            handler.Open();
+        }
+
+        public void Create()
+        {
+            handler.Create();
+        }
+
+        public void Change()
+        {
+            handler.Change();
+        }
+
+        public void Save()
+        {
+            handler.Save();
+        }
+    }
+}
