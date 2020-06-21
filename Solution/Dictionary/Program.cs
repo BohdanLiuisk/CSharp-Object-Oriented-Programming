a68ef21a04d68b69bf7069ebfff21ea39a0bb9a3
BohdanLiusik <bodialiusik@gmail.com>
2020-06-21 22:42:18 +0300

    #1 example of using indexers in c#

---
+++ Solution/Dictionary/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary dictionary = new Dictionary();
+
+            Console.WriteLine(dictionary["book"]);
+            Console.WriteLine(dictionary["дом"]);
+            Console.WriteLine(dictionary["ручка"]);
+            Console.WriteLine(dictionary["стол"]);
+            Console.WriteLine(dictionary["pen"]);
+            Console.WriteLine(dictionary["яблуко"]);
+            Console.WriteLine(dictionary["солнце"]);
+
+            Console.WriteLine(new string('-', 20));
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(dictionary[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
