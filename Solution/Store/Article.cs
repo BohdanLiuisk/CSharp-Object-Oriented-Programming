c4231b65f0d8dd8e46235e4d31c17973a6bd7919
BohdanLiusik <bodialiusik@gmail.com>
2020-07-07 00:45:47 +0300

    class Store using Indexers

---
+++ Solution/Store/Article.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Store
+{
+    class Article
+    {
+        double price;
+
+        public string Name { get; private set; }
+        public string Shop { get; set; }
+
+        public double Price
+        {
+            set
+            {
+                if (value >= 0)
+                    price = value;
+                else
+                    Console.WriteLine("Cost cannot be negative");
+            }
+            get { return price; }
+        }
+
+        public Article(string name)
+        {
+            Name = name;
+        }
+
+        public Article(string name, string shop, double price)
+        {
+            Name = name;
+            Shop = shop;
+            Price = price;
+        }
+
+        public string Info()
+        {
+            return string.Format("Cost of a {0} from {1}: {2} UAN.", Name, Shop, Price);
+        }
+    }
+}
---
+++ Solution/Store/Store.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Store
+{
+    class Store
+    {
+        private Article[] articls;
+
+        public Store(int size)
+        {
+            articls = new Article[Math.Abs(size)];
+        }
+
+        public string this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < articls.Length)
+                    return articls[index].Info();
+                return "An attempt was made to go outside the array.";
+            }
+        }
+
+        public void AddArticle(Article value, int index)
+        {
+            if (index >= 0 && index < articls.Length)
+                articls[index] = value;
+            else
+                Console.WriteLine("An attempt was made to write outside the array.");
+        }
+
+        public string this[string index]
+        {
+            get
+            {
+                for (int i = 0; i < articls.Length; i++)
+                    if (articls[i].Name == index)
+                        return articls[i].Info();
+
+                return string.Format("\"{0}\" not available.", index);
+            }
+        }
+
+        public void Show()
+        {
+            for (int i = 0; i < articls.Length; i++)
+                Console.WriteLine(articls[i].Info());
+        }
+
+        public void Sort()
+        {
+            for (int i = 0; i <= articls.Length - 1; i++)
+            {
+                for (int q = 0; q <= articls.Length - 1; q++)
+                {
+                    if (articls[i].Price <= articls[q].Price)
+                    {
+                        Article g = articls[i];
+                        articls[i] = articls[q];
+                        articls[q] = g;
+                    }
+                }
+            }
+        }
+    }
+}
