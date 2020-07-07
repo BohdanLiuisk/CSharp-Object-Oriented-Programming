7217c5431cd475bc5b33ea9f76d60dea6565594b
BohdanLiusik <bodialiusik@gmail.com>
2020-07-07 15:56:36 +0300

    it shows emaple of delegation

---
+++ Solution/DelegationExample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DelegationExample
+{
+    class Book
+    {
+        public void FindNext(string str)
+        {
+            Console.WriteLine("Search string: " + str);
+        }
+    }
+
+    static class FindAndReplaceManager
+    {
+        static public void FindNext(string str)
+        {
+            Book a = new Book();
+            a.FindNext(str);
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            FindAndReplaceManager.FindNext("Hello");
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
