c231d38edcf2402ad5855b6eb68e5d37f0c048b1
BohdanLiusik <bodialiusik@gmail.com>
2020-07-07 15:31:13 +0300

    static class Calculator

---
+++ Solution/Calculator/Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Calculator
+{
+    static class Calculator
+    {
+        static public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        static public double Subtraction(double a, double b)
+        {
+            return a - b;
+        }
+
+        static public double Multiply(double a, double b)
+        {
+            if (a == 0 || b == 0)
+            {
+                return 0;
+            }
+            return a * b;
+        }
+
+        static public double Division(double a, double b)
+        {
+            if (b == 0)
+            {
+                System.Console.WriteLine("It is impossible to divide by zero!");
+                return 0;
+            }
+            else
+                return a / b;
+        }
+    }
+}
---
+++ Solution/Calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Calculator.Add(9, 71.5));
+            Console.WriteLine(Calculator.Subtraction(90, 4.5));
+            Console.WriteLine(Calculator.Multiply(10, 20));
+            Console.WriteLine(Calculator.Division(10, 0));
+
+            Console.ReadKey();
+        }
+    }
+}
