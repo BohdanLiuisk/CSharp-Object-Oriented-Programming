09063976e0b9c9048e04564c6b61ad6a0d11acb1
BohdanLiusik <bodialiusik@gmail.com>
2020-07-11 23:37:29 +0300

    difference in fields between class and structure

---
+++ Solution/StructAndClass/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace StructAndClass
+{
+    class MyClass
+    {
+        public string change;
+    }
+    
+    struct MyStruct
+    {
+        public string change;
+    }
+
+    class Program
+    {
+        static void ClassTaker(MyClass c)
+        {
+            c.change = "changed";
+        }
+
+        static void StructTaker(MyStruct s)
+        {
+            s.change = "changed";
+        }
+
+        static void Main()
+        {
+            MyClass testClass = new MyClass();
+            MyStruct testStruct = new MyStruct();
+
+            testClass.change = "not changed";
+            testStruct.change = "not changed";
+
+            ClassTaker(testClass);
+            StructTaker(testStruct);
+
+            Console.WriteLine("Class field = {0}", testClass.change);
+            Console.WriteLine("Structure field = {0}", testStruct.change);
+
+            Console.ReadKey();
+        }
+    }
+}
