9298a7bbb67bb58d447aff5bff1ee580a7649aac
BohdanLiusik <bodialiusik@gmail.com>
2020-07-12 23:45:07 +0300

    enum Color

---
+++ Solution/EnumColor/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace EnumColor
+{
+    enum Colors
+    {
+        Blue = 0,
+        Red = 1,
+        Green = 2
+    }
+    static class PrintColor
+    {
+        public static void Print(string line, int color)
+        {
+            switch (color)
+            {
+                case (int)Colors.Blue:
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    break;
+                case (int)Colors.Green:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    break;
+                case (int)Colors.Red:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                default:
+                    Console.WriteLine("The line you entered will be displayed in the default color!");
+                    break;
+            }
+
+            Console.WriteLine(line);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string: ");
+            string line = Console.ReadLine();
+
+            Console.WriteLine("Specify the color (0-blue, 2-green, 1-red): ");
+            int color = Convert.ToInt32(Console.ReadLine());
+
+            PrintColor.Print(line, color);
+        }
+    }
+}
