1ea24385ae59a1995b9abc7a9f8b8725eb0052e5
BohdanLiusik <bodialiusik@gmail.com>
2020-07-12 23:59:34 +0300

    enum post of worker, and class accountant

---
+++ Solution/Accountant/Accountant.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Accountant
+{
+    enum Post
+    {
+        // position and number of hours worked per month
+        Manager = 195,
+        Developer = 200,
+        Accountant = 192,
+        Secretary = 192,
+        Cleaner = 192
+    }
+
+    class Accountant
+    {
+        //a method considering whether to give the employee an award or not,
+        //if the employee has worked more than 192 hours a week - give an award
+        public bool AskForBonus(Post worker, int hours)
+        {
+            if ((int)worker < hours)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Solution/Accountant/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Accountant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of hours worked: ");
+            int hours = Convert.ToInt32(Console.ReadLine());
+
+            Accountant accountant = new Accountant();
+
+            if (accountant.AskForBonus(Post.Cleaner, hours))
+            {
+                Console.WriteLine("Give a bonus");
+            }
+            else
+            {
+                Console.WriteLine("Do not give out a bonus");
+            }
+        }
+    }
+}
