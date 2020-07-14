05d09692c8e4f16edd2d0daf4e77fb9b84a224cd
BohdanLiusik <bodialiusik@gmail.com>
2020-07-15 02:06:02 +0300

    delegates MathOperation and AverageValue

---
+++ Solution/MathOperations/DelegatesExample.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace MathOperations
+{
+    delegate double MathOperations(int a, int b);
+    delegate double AverageValue(int num1, int num2, int num3);
+    class DeletesExample
+    {
+        public static void DoMathOpearations()
+        {
+            Console.WriteLine("Enter first number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter second number: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter an operator(+,-,*,/): ");
+            string z = Convert.ToString(Console.ReadLine());
+
+            MathOperations operation = null;
+            
+            switch (z)
+            {
+                case "+":
+                    operation = (x, y) => { return x + y; };
+                    break;
+                case "-":
+                    operation = (x, y) => { return x - y; };
+                    break;
+                case "*":
+                    operation = (x, y) => { return x * y; };
+                    break;
+                case "/":
+                    operation = (x, y) =>
+                    {
+                        if (y != 0)
+                        {
+                            return x / (double)y;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Can't divide by zero!");
+                            return 0;
+                        }
+                    };
+                    break;
+                default:
+                    Console.WriteLine("Invalid input");
+                    break;
+            }
+            if (operation != null)
+                Console.WriteLine("{0:##.###}", operation(a, b));
+        }
+
+        public static void FindAverageValue()
+        {
+            Console.WriteLine(new string('-', 30));
+            Console.WriteLine("Enter first number: ");
+            int i = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter second number: ");
+            int j = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter third number: ");
+            int k = Convert.ToInt32(Console.ReadLine());
+
+            AverageValue averageValue = (a, b, c) => (double)(a + b + c) / 3;
+            Console.WriteLine("Average value of entered numbers equals {0:##.###}", averageValue(i, j, k));
+        }
+    }
+}
--- Solution/MathOperations/Program.cs
+++ Solution/MathOperations/Program.cs
@@ -1,58 +1,13 @@
 ﻿using System;
 
-namespace MathOperation
+namespace MathOperations
 {
-    
-    delegate int MathOperations(int num1, int num2);
     class Program
     {
-        delegate double MathOperations(int a, int b);
-
         static void Main()
         {
-            Console.WriteLine("Enter first number: ");
-            int a = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Enter second number: ");
-            int b = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Enter an operator(+,-,*,/): ");
-            string z = Convert.ToString(Console.ReadLine());
-
-            MathOperations operation = null;
-
-            switch (z)
-            {
-                case "+":
-                    operation = (x, y) => { return x + y; };
-                    break;
-                case "-":
-                    operation = (x, y) => { return x - y; };
-                    break;
-                case "*":
-                    operation = (x, y) => { return x * y; };
-                    break;
-                case "/":
-                    operation = (x, y) =>
-                    {
-                        if (y != 0)
-                        {
-                            return x / (double)y;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Can't divide by zero!");
-                            return 0;
-                        }
-                    };
-                    break;
-                default:
-                    Console.WriteLine("Invalid input");
-                    break;
-            }
-            Console.WriteLine(new string('-', 30));
-            if (operation != null)
-                Console.WriteLine("{0:##.###}", operation(a, b));
+            DeletesExample.DoMathOpearations();
+            DeletesExample.FindAverageValue();
         }
     }
 }
