b0216d51dbaeb1c2ebf4524d299d7050b05620ff
BohdanLiusik <bodialiusik@gmail.com>
2020-07-17 17:33:45 +0300

    own class Dictionary using Generics

---
+++ Solution/MyDictionaryGenerics/Dictionary.cs
@@ -0,0 +1,37 @@
+﻿namespace MyDictionaryGenerics
+{
+    class Dictionary<TKey, TValue>
+    {
+        private readonly TKey[] key;
+        private readonly TValue[] value;
+        private readonly int lenght;
+
+        public int Lenght
+        {
+            get { return lenght; }
+        }
+
+        public Dictionary(int n)
+        {
+            key = new TKey[n];
+            value = new TValue[n];
+            lenght = n;
+        }
+
+        public string this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < key.Length)
+                    return key[index] + " - " + value[index];
+                return "An attempt to access the array.";
+            }
+        }
+
+        public void Add(int i, TKey k, TValue l)
+        {
+            key[i] = k;
+            value[i] = l;
+        }
+    }
+}
---
+++ Solution/MyDictionaryGenerics/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MyDictionaryGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the dimension of the dictionary: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            var dictionary = new Dictionary<string, string>(n);
+
+            for (int i = 0; i < n; i++)
+            {
+                string word1 = Console.ReadLine();
+                string word2 = Console.ReadLine();
+                dictionary.Add(i, word1, word2);
+            }
+
+            Console.WriteLine(new string('-', 20));
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(dictionary[i]);
+            }
+
+            Console.WriteLine(dictionary[1]);
+            Console.WriteLine(dictionary.Lenght);
+        }
+    }
+}
