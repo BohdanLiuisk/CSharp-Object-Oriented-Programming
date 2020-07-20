8406ae6a0e0f388ed098ddcabcedaac278fcedb9
BohdanLiusik <bodialiusik@gmail.com>
2020-07-20 20:09:25 +0300

    own class Dictionary, Generics

---
+++ Solution/Dictionary2/Dictionary.cs
@@ -0,0 +1,58 @@
+﻿namespace Dictionary2
+{
+    class Dictionary<TKey, TValue>
+    {
+        private readonly MyList<TKey> key;
+        private readonly MyList <TValue> value;
+
+        public Dictionary()
+        {
+            key = new MyList<TKey>();
+            value = new MyList<TValue>();
+        }
+
+        public void AddToDictionary(TKey k, TValue v)
+        {
+            key.Add(k);
+            value.Add(v);
+        }
+
+        public string this[int index]
+        {
+            get { return key[index] + " " + value[index]; }
+        }
+
+        public string this[TKey index]
+        {
+            get
+            {
+                for (int i = 0; i < key.Count; i++)
+                {
+                    if ((string)(object)key[i] == (string)(object)index)
+                    {
+                        return "По ключу " + index.ToString() + " найдено значение: " + value[i].ToString();
+                    }
+                }
+                return "По ключу " + index + " не найдено значения.";
+            }
+        }
+
+        public int Length
+        {
+            get { return key.Count; }
+        }
+
+        public override string ToString()
+        {
+            string str = null;
+            for (int i = 0; i < key.Count; i++)
+            {
+                str += key[i] + " " + value[i] + "\n";
+            }
+
+            if (str != null)
+                return str;
+            return "В словаре нет значений.";
+        }
+    }
+}
---
+++ Solution/Dictionary2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Dictionary2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Словарь:");
+
+            var dictionary = new Dictionary<string, string>();
+
+            dictionary.AddToDictionary("table", "стол");
+            dictionary.AddToDictionary("apple", "яблоко");
+            dictionary.AddToDictionary("sun", "солнце");
+            dictionary.AddToDictionary("pencil", "карандаш");
+            dictionary.AddToDictionary("cap", "чашка");
+            dictionary.AddToDictionary("tea", "чай");
+
+            Console.WriteLine(dictionary.ToString());
+            Console.WriteLine();
+
+            Console.WriteLine("В словаре {0} записей", dictionary.Length);
+            Console.WriteLine(new string('-', 30));
+
+            Console.WriteLine("Введите номер записи в словаре:");
+            string str = Console.ReadLine();
+            if (string.IsNullOrEmpty(str))
+                Console.WriteLine("Вы не ввели номер записи в словаре");
+            else
+            {
+                int number = Convert.ToInt32(str);
+                Console.WriteLine(dictionary[number - 1]);
+            }
+
+            Console.WriteLine("Введите значение для поиска");
+            string p = Console.ReadLine();
+            Console.WriteLine(dictionary[p]);
+        }
+    }
+}
