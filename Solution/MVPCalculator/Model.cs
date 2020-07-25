b724482e7d46d01ce0e113af360c7871e2ba569c
BohdanLiusik <bodialiusik@gmail.com>
2020-07-25 20:55:48 +0300

    calculator by Pattern MVP

---
+++ Solution/MVPCalculator/Model.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MVPCalculator
+{
+    class Model
+    {
+        public string Add(int a, int b)
+        {
+            return (a + b).ToString();
+        }
+        public string Sub(int a, int b)
+        {
+            return (a - b).ToString();
+        }
+        public string Multi(int a, int b)
+        {
+            return (a * b).ToString();
+        }
+        public string Div(int a, int b)
+        {
+            if (b != 0)
+                return ((double) a / b).ToString();
+            return "ти шо дурак, на ноль дилыть";
+        }
+    }
+}
